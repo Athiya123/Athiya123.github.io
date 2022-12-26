1d8c2400741c1793df1e9c152df2ebb117214297
Athiya Fathima <85676437+Athiya123@users.noreply.github.com>
2022-12-26 19:23:25 +0530

    Athiya123.github.io

--- counter.js
+++ counter.js
@@ -6,7 +6,26 @@ function updateCounter() {
      * if count is null then set count to 1
     * else increment the count by 1 (Note: count is a string so convert it to number using parseInt)
     */
+    // var count = 1;
+    // //console.log(count);
+    // if(count == null){
+    //    set.count(1);
+    // }
+    // else{
+    //    count=count+1;
+    // }
+    count = 0;
+    if(count == null){
+        return 1;
+    }
+    count++;
+    
     localStorage.setItem('visiterCount', count);
+
+   
+    // let count = localStorage.getItem('visiterLocation');
+    // localStorage.setItem('visiterLocation', location);
     counter.innerHTML = `Vistor Count: ${localStorage.getItem('visiterCount')}`;
+    //counter.innerHTML = `Vistor Count: ${localStorage.getItem('visiterLocation')}`;
 }
 updateCounter();
