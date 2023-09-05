7a449c19b57457ddaff67bec912ef5cd1399042e
Athiya Fathima <85676437+Athiya123@users.noreply.github.com>
2023-09-05 10:41:25 +0530

    updated portfolio

--- counter.js
+++ counter.js
@@ -1,32 +1,12 @@
 function updateCounter() {
     const counter = document.getElementById('counter');
-    //increment the counter everytime user visits the page by storing in local storage
-    let count = localStorage.getItem('visiterCount');
-    /**
-     * if count is null then set count to 1
-    * else increment the count by 1 (Note: count is a string so convert it to number using parseInt)
-    */
-    // var count = 1;
-    // //console.log(count);
-    // if(count == null){
-    //    set.count(1);
-    // }
-    // else{
-    //    count=count+1;
-    // }
-    if(count == String(null)){
+    let count = localStorage.getItem('visitorCount');
+    if (count === null) {
         count = 1;
-    }
-    else{
+    } else {
         count = parseInt(count) + 1;
     }
-    
-    localStorage.setItem('visiterCount', count);
-
-   
-    // let count = localStorage.getItem('visiterLocation');
-    // localStorage.setItem('visiterLocation', location);
-    counter.innerHTML = `Vistor Count: ${localStorage.getItem('visiterCount')}`;
-    //counter.innerHTML = `Vistor Count: ${localStorage.getItem('visiterLocation')}`;
+    localStorage.setItem('visitorCount', count);
+    counter.innerHTML = `Visitor Count: ${count}`;
 }
 updateCounter();
